d5479e472f4e00ea5cc3f5d53fea3951e4f4c652
Yuri Simantob <ysimantob@student.ethz.ch>
2025-07-04 11:21:44 +0200

    First CrewAI tests with the MCP server

---
+++ agents/api_util.py
@@ -0,0 +1,38 @@
+# !/03_Code/congressMCP/.ba-venv/bin/python
+# util.py
+
+import configparser, os
+from openai import AsyncOpenAI
+from google import genai
+
+
+def __get_api_keys(path="../secrets.ini"):
+    config = configparser.ConfigParser()
+    if not os.path.exists(path):
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        # Build the absolute path to other.txt
+        other_path = os.path.join(script_dir, "..", "secrets.ini")
+        if not os.path.exists(other_path):
+            raise FileNotFoundError(f"Secrets file not found at: {other_path}, because the script was called from {os.getcwd()}")
+        path = other_path
+    config.read(path)
+
+    try:
+        openai_key = config["API_KEYS"]["OPENAI_API_KEY"]
+        gai_key = config["API_KEYS"]["GOOGLE_API_KEY"]
+    except KeyError as e:
+        raise KeyError(f"Missing expected key in secrets.ini: {e}")
+
+    return openai_key, gai_key
+
+def _get_oai_client():
+    _, openai_key, _ = __get_api_keys()
+
+    oai_client = AsyncOpenAI(api_key=openai_key)
+
+    return oai_client
+
+def _get_gai_client():
+    _, _, gai_key = __get_api_keys()
+    gai_client = genai.Client(api_key=gai_key)
+    return gai_client
